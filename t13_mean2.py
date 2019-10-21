4c834234fcfa4f78db2ff0a1b68fd5b1d8f484e1
dhslimyeening <38546916+dhslimyeening@users.noreply.github.com>
2019-10-21 14:29:56 +0800

    Update t13_mean2.py

--- t13_mean2.py
+++ t13_mean2.py
@@ -4,9 +4,12 @@ mean = 52
 
 known_numbers = [34, 23, 65, 91]
 
+# Calculate the total sum of the set of numbers
 total = mean * 5
 
+# Calculate the value of x
 for num in known_numbers:
   total -= num
 
+  # Print value of x
 print(total)
