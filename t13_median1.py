fdaf53693694202052f9226842a64025d4731c35
dhslimwencyclarice <38546137+dhslimwencyclarice@users.noreply.github.com>
2019-10-21 14:28:19 +0800

    Update t13_median1.py

--- t13_median1.py
+++ t13_median1.py
@@ -3,6 +3,7 @@ A = [4, 1, 5, 6, 2, 5]
 
 A.sort()
 print(A)
+A = [1, 2, 4, 5, 5, 6]
 
 mid = len(A) // 2
 if len(A) % 2 == 0: # even n, take average of middle 2 values
@@ -10,3 +11,4 @@ if len(A) % 2 == 0: # even n, take average of middle 2 values
 else: # odd n
   median = A[mid]
 print(median)
+median = 4.5
