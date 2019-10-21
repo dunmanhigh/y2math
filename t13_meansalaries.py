1b4cb3835dfcc4c2a68cd599092801178d1606ba
dhslowjingyistephy <38546894+dhslowjingyistephy@users.noreply.github.com>
2019-10-21 14:23:31 +0800

    Annotations for easier understanding of code

--- t13_meansalaries.py
+++ t13_meansalaries.py
@@ -1,13 +1,17 @@
 # calculate mean salary
-
 weekly_salaries = [1000, 1100, 1200, 2100, 2500]
-
 num_employees = [20, 8, 10, 7, 5]
+
+# Calculate the number of employees and display it as "Total number of employees"
 print("Total number of employees:", sum(num_employees))
 
+# Calculate the total amount of salary
 total_salaries = 0
 for i in range(len(weekly_salaries)):
   total_salaries += weekly_salaries[i] * num_employees[i]
+
+# Display total salaries under "Total salary paid"
 print("Total salary paid:", total_salaries)
 
+# Calculate mean weekly salary based on values found in previous equations
 print("Mean weekly salary:", total_salaries / sum(num_employees))
