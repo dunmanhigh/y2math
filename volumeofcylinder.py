b095a0dfdb277705718af9c90f9925aaa0e0daa8
dhsngochongxi <38057056+dhsngochongxi@users.noreply.github.com>
2019-10-21 14:15:43 +0800

    # find volume of cylinder

---
+++ volumeofcylinder.py
@@ -0,0 +1,9 @@
+
+# find volume of cylinder
+import math
+r=float(input("Enter r of a cylinder"))
+h=float(input("Enter the Height of a cylinder"))
+s_area=2*math.pi*pow(r,2)*h
+volume=math.pi*pow(r,2)*h
+print("surface area of a cylinder wll be %.2f" %s_area)
+print("volume of a cylinder will be  %.2f" %volume)
