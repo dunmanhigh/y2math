3ffafaa48574bfdbaf1e7b600a16d2bc9b2b4aab
dhssunyutong <38057108+dhssunyutong@users.noreply.github.com>
2019-10-21 14:08:05 +0800

    Update t02_simultaneouslinearequations.py

--- t02_simultaneouslinearequations.py
+++ t02_simultaneouslinearequations.py
@@ -1,3 +1,10 @@
 # Find x and y with these two equations:
-# 3x + 4y = 24
-# 4x + 3y = 22
+# 3x + 4y = 24 -- (1)
+# 4x + 3y = 22 -- (2)
+from (1), 12x + 16y = 96 -- (3)
+from (2), 12x + 9y = 66 -- (4)
+(3)-(4): 7y = 30
+y = 4.2857 (5sf) -- (5)
+sub (5) into (1): 3x + 4(4.2857) =24
+  x = 2.29 (3sf)
+hence x = 2.29 and y = 4.29
