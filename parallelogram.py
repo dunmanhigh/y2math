01637766b33aca201568cb94d97fbd6c6062a95f
yuhaodhs <38057141+yuhaodhs@users.noreply.github.com>
2019-10-21 14:09:37 +0800

    Create parallelogram.py

---
+++ parallelogram.py
@@ -0,0 +1,9 @@
+#find the area of a parellelogram using the formula of area of parellelogram
+
+formula=  base = float(input('Length of base: '))
+          height = float(input('Measurement of height: '))
+          area = base * height
+          print("Area is: ", area)
+
+base = 9
+height = 5
