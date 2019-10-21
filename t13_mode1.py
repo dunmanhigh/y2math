7b6120c4d9287fed619400fbc953ccc75c16cd4b
dhsjanelletan <38057030+dhsjanelletan@users.noreply.github.com>
2019-10-21 13:49:24 +0800

    Update Math Question

--- t13_mode1.py
+++ t13_mode1.py
@@ -6,4 +6,5 @@ A = [19, 18, 46, 18, 18, 19]
 print(statistics.mode(A))
 
 # the above only works if there is only 1 mode
-# what if there is more than 1 mode?
+# what is the mode of the numbers?
+# Calculate the mean of the numbers
