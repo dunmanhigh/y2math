511a1c2a563986c996562aa7f8d856fe0a7034ca
Bea05RJ <44458363+Bea05RJ@users.noreply.github.com>
2019-10-22 14:19:39 +0800

    t13_areaofcylinder1.py
    Koh Beatrix 2H Area of cylinder

---
+++ t13_areaofcylinder1.py
@@ -0,0 +1,7 @@
+diameter = float(input("Enter the diameter of cylinder: "))
+radius = diameter/2
+pi = 3.1415
+height = float(input("Enter the height of cylinder: "))
+
+area_of_cylinder = pi*(radius**2)*height
+print(area_of_cylinder)
