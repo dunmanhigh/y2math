fdafd62be8d995943ac04686d17b1688e3a048eb
dhsgoiruoen <38025508+dhsgoiruoen@users.noreply.github.com>
2019-10-22 14:10:50 +0800

    Goi Ruo En 2H

--- t13_median1.py
+++ t13_median1.py
@@ -10,3 +10,17 @@ if len(A) % 2 == 0: # even n, take average of middle 2 values
 else: # odd n
   median = A[mid]
 print(median)
+
+
+# Find median of n numbers
+A = [1, 2, 3, 4, 5, 6, 7]
+
+A.sort()
+print(A)
+
+mid = len(A) // 2
+if len(A) % 2 == 0: # even n, take average of middle 2 values
+  median = (A[mid-1] + A[mid]) / 2
+else: # odd n
+  median = A[mid]
+print(median)
