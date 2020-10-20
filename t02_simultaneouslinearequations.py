0370c69da4067d32ac7434d480b570f96be8d66f
YONGS13 <52412571+YONGS13@users.noreply.github.com>
2020-10-20 14:03:05 +0800

    Hacktoberfest-accepted
    I have solved the system of linear equations. Pls review my answer. Thanks!

--- t02_simultaneouslinearequations.py
+++ t02_simultaneouslinearequations.py
@@ -1,3 +1,11 @@
 # Find x and y with these two equations:
 # 3x + 4y = 24
 # 4x + 3y = 22
+4y=24-3x
+y=(-3/4)x+6
+#substituting y in the second equation yields:
+ 4x+3((-3/4)x+6)=22
+(4-9/4)x  =22-18
+7/4x=4
+x=4 *(4/7)=16/7
+y=(-3/4)(16/7)+6=-48/28+6=-12/7+42/7=30/7
